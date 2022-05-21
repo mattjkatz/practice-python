c35ff05d8acb141d3195231f2a42abcf1e89562c
mattjkatz <matthewkatz7@gmail.com>
2022-05-20 23:51:12 -0500

    second problem woowoo

--- practice.py
+++ practice.py
@@ -1,8 +1,21 @@
-# Find the sum of all the numbers in an array
-def sumCalculator(ar):
-  sum = 0
-  for i in ar:
-    sum += i
-  return sum
+# # Find the sum of all the numbers in an array
 
-print(sumCalculator([3, 4, 6, 2, 9]))
+# def sumCalculator(ar):
+#   sum = 0
+#   for i in ar:
+#     sum += i
+#   return sum
+
+# print(sumCalculator([3, 4, 6, 2, 9]))
+
+
+# Find the largest number in the array
+
+def largestNum(ar):
+  largest = ar[0]
+  for i in ar:
+    if (i > largest):
+      largest = i
+  return largest
+
+print(largestNum([5, 67, 89, 34, 12, 96, 6]))
