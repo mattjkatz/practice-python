83a72655429128cbd46ede303a1e315f21f9289b
mattjkatz <matthewkatz7@gmail.com>
2022-05-21 00:38:12 -0500

    more python practice

--- practice.py
+++ practice.py
@@ -9,13 +9,26 @@
 # print(sumCalculator([3, 4, 6, 2, 9]))
 
 
-# Find the largest number in the array
+# # Find the largest number in the array
 
-def largestNum(ar):
-  largest = ar[0]
-  for i in ar:
-    if (i > largest):
-      largest = i
-  return largest
+# def largestNum(ar):
+#   largest = ar[0]
+#   for i in ar:
+#     if (i > largest):
+#       largest = i
+#   return largest
 
-print(largestNum([5, 67, 89, 34, 12, 96, 6]))
+# print(largestNum([5, 67, 89, 34, 12, 96, 6]))
+
+
+# Print an array reversed
+
+def reverseList(ar):
+  ar_reversed = []
+  i = len(ar) - 1
+  while i >= 0:
+    ar_reversed.append(ar[i])
+    i -= 1
+  return ar_reversed
+
+print(reverseList([2, 5, 3, 8, 11]))
