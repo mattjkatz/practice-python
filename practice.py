4604a1f5d255535b3e463b65c170e8327a7ad69c
mattjkatz <matthewkatz7@gmail.com>
2022-05-29 19:16:59 -0500

    uncomment all functions and only comment descriptions and print statements

--- practice.py
+++ practice.py
@@ -1,22 +1,22 @@
 # # Find the sum of all the numbers in an array
 
-# def sumCalculator(ar):
-#   sum = 0
-#   for i in ar:
-#     sum += i
-#   return sum
+def sumCalculator(ar):
+  sum = 0
+  for i in ar:
+    sum += i
+  return sum
 
 # print(sumCalculator([3, 4, 6, 2, 9]))
 
 
 # # Find the largest number in the array
 
-# def largestNum(ar):
-#   largest = ar[0]
-#   for i in ar:
-#     if (i > largest):
-#       largest = i
-#   return largest
+def largestNum(ar):
+  largest = ar[0]
+  for i in ar:
+    if (i > largest):
+      largest = i
+  return largest
 
 # print(largestNum([5, 67, 89, 34, 12, 96, 6]))
 
@@ -36,51 +36,54 @@ def reverseList(ar):
 
 # # print the first 100 digits of the fibbonaci sequence
 
-# def fibbonaci():
-#   a = 0
-#   b = 1
-#   print(a)
-#   print(b)
-#   for i in range(1, 100):
-#     c = a + b
-#     print(c)
-#     a = b
-#     b = c
+def fibbonaci():
+  a = 0
+  b = 1
+  print(a)
+  print(b)
+  for i in range(1, 100):
+    c = a + b
+    print(c)
+    a = b
+    b = c
 
 # fibbonaci()
 
 
 # # Remove duplicates from an array
 
-# def removeDuplicates(ar):
-#   compAr = []
-#   for i in ar:
-#     duplicate = False
-#     for j in compAr:
-#       if (i == j):
-#         duplicate = True
-#     if (duplicate == False):
-#       compAr.append(i)
-#   return compAr
+def removeDuplicates(ar):
+  compAr = []
+  for i in ar:
+    duplicate = False
+    for j in compAr:
+      if (i == j):
+        duplicate = True
+    if (duplicate == False):
+      compAr.append(i)
+  return compAr
 
 # print(removeDuplicates([2, 5, 7, 3, 5, 89, 3, 1, 5]))
 
 
 # # Find the two largest numbers
 
-# def twoLargestNum(ar):
-#   lg = ar[0]
-#   second_lg = lg
-#   for i in ar:
-#     if (i > lg):
-#       second_lg = lg
-#       lg = i
-#     elif (i > second_lg):
-#       second_lg = i
-#   print(lg)
-#   print(second_lg)
+def twoLargestNum(ar):
+  lg = ar[0]
+  second_lg = lg
+  for i in ar:
+    if (i > lg):
+      second_lg = lg
+      lg = i
+    elif (i > second_lg):
+      second_lg = i
+  print(lg)
+  print(second_lg)
 
 # twoLargestNum([2, 5, 23, 67, 8, 9, 1, 4, 34])
+
+
+# return a string of a number with commas ever third digit
 
 def numAddCommas(int):
   int_split = reverseList(list(str(int)))
@@ -96,4 +99,4 @@ def numAddCommas(int):
   else:
     print(int)
 
-numAddCommas(5000/3)
+# numAddCommas(5000/3)
